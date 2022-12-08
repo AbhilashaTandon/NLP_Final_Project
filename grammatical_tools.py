5aaa549a19a3843522feb76fb072dffe455bc09c
Abhilasha Tandon <113814700+AbhilashaTandon@users.noreply.github.com>
2022-12-08 18:45:09 -0500

    Refactored code to random_forest.py
    I took the code in the notebook file sentiment.ipynb and copy pasted it to a new python file and changed a couple things to use existing file parsing stuff I already wrote.

--- grammatical_tools.py
+++ grammatical_tools.py
@@ -5,7 +5,7 @@ import nltk
 stops = set(stopwords.words('english'))
 
 stops_from_tokenization = set(["'d", "'ll", "'re", "'s", "'ve", 'could', 'might', 'must', 'need', 'sha', 'wo', 'would'])
-negative_words = set(["nobody", "nothing", "never", "no", "won't", "not", "don't", "isn't", "can't", "wouldn't", "didn't", "ain't", "shouldn't", "couldn't", "mustn't"])
+negative_words = set(["n't", "nobody", "nothing", "never", "no", "won't", "not", "don't", "isn't", "can't", "wouldn't", "didn't", "ain't", "shouldn't", "couldn't", "mustn't"])
 stops = (stops | stops_from_tokenization) - negative_words
 
 #print(stops)
@@ -15,7 +15,7 @@ from nltk.tag import pos_tag
 
 def tokenizer(text):
 	tokenized = [word for word in nltk.word_tokenize(text)]
-	return (tokenized, [[word,''][word in stops] for word in tokenized])
+	return (tokenized, [("" if word in stops else word) for word in tokenized])
 
 def word_split(text):
 	return [word for word in text.split() if word not in stops]
@@ -30,6 +30,20 @@ def chunker(sentence, regex):
 	chunker = nltk.RegexpParser(regex)
 	chunked = chunker.parse(pos_tagged)
 	print(chunked)
+
+def negation_analyzer(text): #predicts what words are negated semantically
+	splitted = word_split(text)
+	polarities = [1 for word in splitted]
+	for idx, word in enumerate(splitted):
+		if(word in stops or word in negative_words):
+			polarities[idx] = 0
+		if(idx != 0):
+			if(splitted[idx-1] in negative_words):
+				polarities[idx] *= -1
+	word_pols = {word: 0 for word in set(splitted)}
+	for pol, word in zip(polarities, splitted):
+		word_pols[word] += pol
+	return word_pols
 
 from file_parsing import parse_file
 
@@ -46,7 +60,9 @@ train_all = shuffle(train_pos + train_neg)
 
 def main():
 	for sent in train_all[:100]:
-		print(word_split(sent[1]))
+		splitted = set(word_split(sent[1]))
+		if(splitted | negative_words != set()):
+			print(sent)
 
 if __name__ == '__main__':
 	main()
--- lda.py
+++ lda.py
@@ -1,10 +1,8 @@
 import numpy as np
-from nltk.corpus import stopwords
 import nltk
-stops = set(stopwords.words('english'))
 
 from file_parsing import parse_file, rating_review_split
-from grammatical_tools import stops, word_split
+from grammatical_tools import stops, word_split, negation_analyzer
 
 def shuffle(list):
 	np.random.shuffle(list)
@@ -20,11 +18,17 @@ train_neg = parse_file('train_neg_reviews.txt')
 dev_pos = parse_file('dev_pos_reviews.txt')
 dev_neg = parse_file('dev_neg_reviews.txt')
 
+
 print(len(train_pos), len(train_neg), len(dev_pos), len(dev_neg))
 
 #shuffles together positive and negative reviews
 train_all = shuffle(train_pos + train_neg)
 dev_all = shuffle(dev_pos + dev_neg)
+
+num_topics = 5
+num_train_reviews = len(train_all)
+num_dev_reviews = len(dev_all)
+num_words = len(all_vocabulary)
 
 train_ratings, train_reviews = rating_review_split(train_all)
 dev_ratings, dev_reviews = rating_review_split(dev_all)
@@ -35,6 +39,9 @@ from scipy import stats
 
 normed_train_ratings = stats.zscore(train_ratings, axis=None)
 normed_dev_ratings = stats.zscore(dev_ratings, axis=None)
+
+train_polarities = [2*int(rating >= 7)-1 for rating in train_ratings]
+dev_polarities = [2*int(rating >= 7)-1 for rating in dev_ratings]
 #uses z-scores to normalize ratings, (rating - mean)/stdev
 
 def get_tf_idfs(doc_list): #calculates tf-idf scores for each doc, returns those and a list of words in all docs
@@ -54,36 +61,46 @@ def get_tf_idfs(doc_list): #calculates tf-idf scores for each doc, returns those
 
 from sklearn.decomposition import LatentDirichletAllocation
 
-def lda(tf_idfs, topics, test_tf_idfs): #does latent dirichlet analysis on tf-idf scores to find topics
+def lda(tf_idfs, topics): #does latent dirichlet analysis on tf-idf scores to find topics
 	lda = LatentDirichletAllocation(n_components=topics, random_state=0)
 	doc_topics = lda.fit_transform(tf_idfs)
-	test_doc_topics = lda.transform(test_tf_idfs)
-	return lda.components_, doc_topics, test_doc_topics
+	return lda.components_, doc_topics
 
 from sklearn.linear_model import LinearRegression
 
-def find_polarity_topics(doc_topics, review_polarities): 
+def find_polarity_topics(doc_topics, review_polarities):
 	#linear regression with array of topic scores (X) and polarities (Y)
 	reg = LinearRegression().fit(doc_topics, review_polarities)
 	print(reg.score(doc_topics,review_polarities))
 	return reg.coef_, reg.intercept_
+
+
+def lda_transform(topic_weights, tf_idfs, texts): #uses topic weights from previous lda to find topics from tf-idfs
+	return np.dot(topic_weights, tf_idfs)
 	
+	negation_masks = np.array(negation_analyzer(texts)).reshape(tf_idfs.shape)
+	return np.dot(topic_weights, np.multiply(tf_idfs, negation_masks))
+	
+
 def predict_polarity(coeffs, intercept, doc_topics): #uses topic weights to predict polarity from topics
-	return np.dot(coeffs, doc_topics) + intercept 
+	return np.dot(coeffs, doc_topics) + intercept
 
 def main():
 	train_tf_idfs = get_tf_idfs(train_all) #get training data tf-idfs
 	
 	dev_tf_idfs = get_tf_idfs(dev_all) #get development data tf-idfs
 	
-	topic_weights, doc_topics, test_doc_topics = lda(train_tf_idfs, 100, dev_tf_idfs)
+	
+	topic_weights, doc_topics = lda(train_tf_idfs, num_topics)
 	#do lda on tf-idfs and apply the results to get topics for dev set
 	
-	coefs, y_intercept = find_polarity_topics(doc_topics, normed_train_ratings)
+	test_doc_topics = lda_transform(topic_weights, dev_tf_idfs, dev_reviews)
+	
+	coefs, y_intercept = find_polarity_topics(doc_topics, train_polarities)
 	#do linear regression to predict polarity from topics
 	
 	predicted_polarities = [predict_polarity(coefs, y_intercept, topics) > 0 for topics in test_doc_topics]
-	actual_polarities = [rating >= 7 for rating in dev_ratings]
+	actual_polarities = [rating >= 7 for rating in dev_polarities]
 	#convert sliding scale to discrete
 	
 	from sklearn.metrics import precision_score, accuracy_score, recall_score
@@ -99,7 +116,6 @@ def main():
 	from sklearn.metrics import confusion_matrix
 	
 	print(confusion_matrix(actual_polarities, predicted_polarities))
-	
 
 if __name__ == '__main__':
 	main()
