b32f7dcd9365018e4d8261463558af7eba1fd5b1
Abhilasha Tandon <113814700+AbhilashaTandon@users.noreply.github.com>
2022-12-23 16:38:07 -0500

    Various Updates
    Removed blank lines from the end of the data files, and added a couple more files

--- lda.py
+++ lda.py
@@ -10,13 +10,18 @@ def shuffle(list):
 
 all_vocabulary = open("aclImdb_v1\\aclImdb\\imdb.vocab", encoding = 'utf-8').read().splitlines()
 
+#all_vocabulary = all_vocabulary[:50000]
+
 #consider limiting the vocabulary to the first n terms, since these are the most common n terms
 
 #lists for storing documents
 train_pos = parse_file('train_pos_reviews.txt')
 train_neg = parse_file('train_neg_reviews.txt')
-dev_pos = parse_file('dev_pos_reviews.txt')
-dev_neg = parse_file('dev_neg_reviews.txt')
+dev_pos = parse_file('test_pos_reviews.txt')
+dev_neg = parse_file('test_neg_reviews.txt')
+
+#dev_pos = parse_file('dev_pos_reviews.txt')
+#dev_neg = parse_file('dev_neg_reviews.txt')
 
 
 print(len(train_pos), len(train_neg), len(dev_pos), len(dev_neg))
@@ -25,7 +30,7 @@ print(len(train_pos), len(train_neg), len(dev_pos), len(dev_neg))
 train_all = shuffle(train_pos + train_neg)
 dev_all = shuffle(dev_pos + dev_neg)
 
-num_topics = 5
+num_topics = 1000
 num_train_reviews = len(train_all)
 num_dev_reviews = len(dev_all)
 num_words = len(all_vocabulary)
@@ -61,10 +66,11 @@ def get_tf_idfs(doc_list): #calculates tf-idf scores for each doc, returns those
 
 from sklearn.decomposition import LatentDirichletAllocation
 
-def lda(tf_idfs, topics): #does latent dirichlet analysis on tf-idf scores to find topics
+def lda(tf_idfs, topics, test_tf_idfs): #does latent dirichlet analysis on tf-idf scores to find topics
 	lda = LatentDirichletAllocation(n_components=topics, random_state=0)
 	doc_topics = lda.fit_transform(tf_idfs)
-	return lda.components_, doc_topics
+	test_topics = lda.transform(test_tf_idfs)
+	return lda.components_, doc_topics, test_topics
 
 from sklearn.linear_model import LinearRegression
 
@@ -75,12 +81,12 @@ def find_polarity_topics(doc_topics, review_polarities):
 	return reg.coef_, reg.intercept_
 
 
-def lda_transform(topic_weights, tf_idfs, texts): #uses topic weights from previous lda to find topics from tf-idfs
-	return np.dot(topic_weights, tf_idfs)
-	
-	negation_masks = np.array(negation_analyzer(texts)).reshape(tf_idfs.shape)
-	return np.dot(topic_weights, np.multiply(tf_idfs, negation_masks))
-	
+#def lda_transform(topic_weights, tf_idfs, texts): #uses topic weights from previous lda to find topics from tf-idfs
+#	return np.dot(topic_weights, tf_idfs)
+#	
+#	negation_masks = np.array(negation_analyzer(texts)).reshape(tf_idfs.shape)
+#	return np.dot(topic_weights, np.multiply(tf_idfs, negation_masks))
+#	
 
 def predict_polarity(coeffs, intercept, doc_topics): #uses topic weights to predict polarity from topics
 	return np.dot(coeffs, doc_topics) + intercept
@@ -91,31 +97,25 @@ def main():
 	dev_tf_idfs = get_tf_idfs(dev_all) #get development data tf-idfs
 	
 	
-	topic_weights, doc_topics = lda(train_tf_idfs, num_topics)
+	topic_weights, doc_topics, test_doc_topics = lda(train_tf_idfs, num_topics, dev_tf_idfs)
 	#do lda on tf-idfs and apply the results to get topics for dev set
 	
-	test_doc_topics = lda_transform(topic_weights, dev_tf_idfs, dev_reviews)
+	# = lda_transform(topic_weights, dev_tf_idfs, dev_reviews)
 	
 	coefs, y_intercept = find_polarity_topics(doc_topics, train_polarities)
 	#do linear regression to predict polarity from topics
 	
 	predicted_polarities = [predict_polarity(coefs, y_intercept, topics) > 0 for topics in test_doc_topics]
-	actual_polarities = [rating >= 7 for rating in dev_polarities]
+	actual_polarities = [rating >= 0 for rating in dev_polarities]
 	#convert sliding scale to discrete
 	
 	from sklearn.metrics import precision_score, accuracy_score, recall_score
 	
 	acc, pre, rec = accuracy_score(actual_polarities, predicted_polarities), precision_score(actual_polarities, predicted_polarities), recall_score(actual_polarities, predicted_polarities)
 	
-	#print out statistical measures
-	print('accuracy %%%.2f' % (100*acc))
-	print('precision %%%.2f' % (100*pre))
-	print('recall %%%.2f' % (100*rec))
-	print('f-score %%%.2f' % (100* (2/(1/pre + 1/rec))))
+	from sklearn.metrics import classification_report
 	
-	from sklearn.metrics import confusion_matrix
-	
-	print(confusion_matrix(actual_polarities, predicted_polarities))
+	print(classification_report(actual_polarities, predicted_polarities))
 
 if __name__ == '__main__':
 	main()
